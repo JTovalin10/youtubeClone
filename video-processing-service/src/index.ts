d86691dfd981a21dcf4963b805b90bd3dd409883
Justin Hernandez Tovalin <justinhernlin@icloud.com>
2025-10-08 01:06:44 -0700

    finished adding google cloud storage for videos

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,35 +1,46 @@
 import express from 'express';
-import ffmpeg from 'fluent-ffmpeg';
+import { uploadProcessedVideo, downloadRawVideo, deleteRawVideo, deleteProcessedVideo, convertVideo, setupDirectories } from './storage'
 
-const app = express();
+setupDirectories();
+
+const app = express()
 app.use(express.json());
 
-app.post('/process-video', (req, res) => {
-
-  // Get the path of the input video file from the request body
-  const inputFilePath = req.body.inputFilePath;
-  const outputFilePath = req.body.outputFilePath;
-
-  // Check if the input file path is defined
-  if (!inputFilePath || !outputFilePath) {
-    return res.status(400).send('Bad Request: Missing file path');
+app.post('/process-video', async (req, res) => {
+  let data;
+  try {
+    const message = Buffer.from(req.body.message, 'base64').toString('utf8');
+    data = JSON.parse(message)
+    if (!data.name) {
+      throw new Error("Invalid message payload recieved")
+    }
+  } catch (error) {
+    console.error(error)
+    return res.status(400).send("Bad message: missing filename");
   }
 
-  // Create the ffmpeg command
-  ffmpeg(inputFilePath)
-    .outputOptions('-vf', 'scale=-1:360') // 360p
-    .on('end', function() {
-        console.log('Processing finished successfully');
-        res.status(200).send('Processing finished successfully');
-    })
-    .on('error', function(err: any) {
-        console.log('An error occurred: ' + err.message);
-        res.status(500).send('An error occurred: ' + err.message);
-    })
-    .save(outputFilePath);
-});
+  const inputFileName = data.name;
+  const outputFileName = `processed-${inputFileName}`
 
-const port = process.env.PORT || 3000;
+  await downloadRawVideo(inputFileName)
+
+  try {
+    await convertVideo(inputFileName, outputFileName)
+  } catch (errror) {
+    deleteRawVideo(inputFileName);
+    deleteProcessedVideo(outputFileName);
+  }
+
+  await uploadProcessedVideo(outputFileName);
+
+  await Promise.all([
+    deleteRawVideo(inputFileName),
+    deleteProcessedVideo(outputFileName)
+  ]);
+  return res.status(200).send("Processing finished successfully")
+})
+
+const port = process.env.PORT || 3000
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+  console.log(`Server is running at port ${port}`)
+})
